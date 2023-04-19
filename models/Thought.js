ebb367bebff8f1a6c7910508751b924360910a74
Jeremy McCarter <jeremymccarter27@gmail.com>
2023-04-19 11:41:19 -0700

    set up some file structure and started on thought model/reaction submodel

---
+++ models/Thought.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require('mongoose');
+
+
+
+const reactionSchema= new Schema(
+{
+reactionID: {
+    type: Schema.Types.ObjectId,
+    default: function() {
+        return new Schema.Types.ObjectId()
+    }
+
+},
+reactionBody: {
+    type: String,
+    required: true,
+    max_length: 280,
+},
+username: {
+    type: String,
+    required: true,
+},
+createdAt: {
+    type: Date,
+    get: function(createdAt){
+        return createdAt.toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"})
+    }
+}
+
+
+
+})
